a70c51f60d29797259c90849a938b225cf50130d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2026-03-16 17:55:13 +0100

    refactor: add QCPColormapRenderer header (extracted from QCPColorMap2)

---
+++ src/painting/colormap-renderer.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <colorgradient.h>
+#include <axis/axis.h>
+#include <QImage>
+#include <functional>
+
+class QCPAbstractPlottable;
+class QCPColorScale;
+class QCPColorMapData;
+class QCPColormapRhiLayer;
+class QCPPainter;
+class QCustomPlot;
+
+class QCPColormapRenderer
+{
+public:
+    explicit QCPColormapRenderer(QCPAbstractPlottable* owner);
+    ~QCPColormapRenderer();
+
+    // State
+    void setGradient(const QCPColorGradient& gradient);
+    const QCPColorGradient& gradient() const { return mGradient; }
+
+    void setDataRange(const QCPRange& range);
+    QCPRange dataRange() const { return mDataRange; }
+
+    void setDataScaleType(QCPAxis::ScaleType type);
+    QCPAxis::ScaleType dataScaleType() const { return mDataScaleType; }
+
+    void setColorScale(QCPColorScale* scale);
+    QCPColorScale* colorScale() const { return mColorScale; }
+
+    void rescaleDataRange(const QCPColorMapData* data, bool recalc);
+
+    // Rendering
+    using NormalizeFn = std::function<double(double value, int col, int row)>;
+    void updateMapImage(const QCPColorMapData* data, NormalizeFn normalize = {});
+    void draw(QCPPainter* painter, QCPAxis* keyAxis, QCPAxis* valueAxis,
+              const QCPRange& keyRange, const QCPRange& valueRange);
+
+    // RHI layer
+    QCPColormapRhiLayer* ensureRhiLayer();
+    void releaseRhiLayer();
+
+    // Image cache
+    bool mapImageInvalidated() const { return mMapImageInvalidated; }
+    void invalidateMapImage() { mMapImageInvalidated = true; }
+    const QImage& mapImage() const { return mMapImage; }
+
+private:
+    QCPAbstractPlottable* mOwner;
+    QCPColorGradient mGradient;
+    QCPRange mDataRange;
+    QCPAxis::ScaleType mDataScaleType = QCPAxis::stLinear;
+    QImage mMapImage;
+    bool mMapImageInvalidated = true;
+    QCPColorScale* mColorScale = nullptr;
+    QCPColormapRhiLayer* mRhiLayer = nullptr;
+};
